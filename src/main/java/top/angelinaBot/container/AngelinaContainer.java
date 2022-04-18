3a00cafa104845da8b183df5f55d9746cf14cfb6
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-04-18 09:48:01 +0800

    补充方法反射容器注释

--- src/main/java/top/angelinaBot/container/AngelinaContainer.java
+++ src/main/java/top/angelinaBot/container/AngelinaContainer.java
@@ -11,18 +11,18 @@ import java.util.Map;
 /**
  * @author strelitzia
  * @Date 2022/04/03
- * 存放所有@Angelina注解的方法映射。
+ * 存放所有@Angelina注解的方法映射的反射容器。
  **/
 @Component
 public class AngelinaContainer {
-    //用于存放所有被注解的方法
+    //群聊的关键字触发方法
     public static final Map<String, Method> groupMap = new HashMap<>();
-
+    //闲聊的关键字触发方法，闲聊配置仅能在群聊中触发
     public static final Map<String, List<String>> chatMap = new HashMap<>();
-
+    //dHash图片触发方法，仅能在群聊中触发
     public static final Map<String, Method> dHashMap = new HashMap<>();
-
+    //私聊的关键字触发方法
     public static final Map<String, Method> friendMap = new HashMap<>();
-
+    //事件触发方法
     public static final Map<EventEnum, Method> eventMap = new HashMap<>();
 }
