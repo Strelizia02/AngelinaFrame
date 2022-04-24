261a154afcf271bd08ad1da71384072a7776e79d
Strelitzia <Strelitzia>
2022-04-24 22:11:33 +0800

    创建数据库文件

--- src/main/java/top/angelinaBot/config/LocalDataSourceConfig.java
+++ src/main/java/top/angelinaBot/config/LocalDataSourceConfig.java
@@ -1,5 +1,6 @@
 package top.angelinaBot.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
@@ -8,12 +9,16 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.sqlite.SQLiteDataSource;
 
 import javax.sql.DataSource;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author strelitzia
@@ -22,13 +27,27 @@ import java.io.File;
 @Configuration
 @MapperScan(basePackages = "top.angelinaBot.dao",
         sqlSessionTemplateRef = "angelinaSqlSessionTemplate")
+@Slf4j
 public class LocalDataSourceConfig {
     /**
      * 创建数据源
      */
     @Bean(name = "angelinaDataSource")
     public DataSource getFirstDataSource() {
-        File file = new File("src/main/resources/sqlite/template.db");
+        File file = new File("runFile/template.db");
+        if (!file.exists()) {
+            if (file.mkdirs()) {
+                ClassPathResource classPathResource = new ClassPathResource("/sqlite/template.db");
+                try(InputStream is = classPathResource.getInputStream();FileOutputStream fs = new FileOutputStream(file)) {
+                    byte[] b = new byte[1024];
+                    while (is.read(b) != -1) {
+                        fs.write(b);
+                    }
+                } catch (IOException e) {
+                    log.error("创建数据库文件失败");
+                }
+            }
+        }
         return DataSourceBuilder.create()
                 .driverClassName("org.sqlite.JDBC")
                 .url("jdbc:sqlite:" + file.getAbsolutePath())
