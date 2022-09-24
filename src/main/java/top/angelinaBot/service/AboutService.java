e315d3dcdde6bd04f8c1c3878964afaae4d88348
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-09-24 17:25:23 +0800

    Update AboutService.java

--- src/main/java/top/angelinaBot/service/AboutService.java
+++ src/main/java/top/angelinaBot/service/AboutService.java
@@ -11,7 +11,7 @@ public class AboutService {
     @AngelinaGroup(keyWords = {"关于"}, description = "框架地址源码")
     public ReplayInfo getAbout(MessageInfo messageInfo) {
         ReplayInfo replayInfo = new ReplayInfo(messageInfo);
-        replayInfo.setReplayMessage("框架主页:https://www.angelina-bot.top/");
+        replayInfo.setReplayMessage(new String(new byte[]{-26, -95, -122, -26, -98, -74, -28, -72, -69, -23, -95, -75, 58, 104, 116, 116, 112, 115, 58, 47, 47, 119, 119, 119, 46, 97, 110, 103, 101, 108, 105, 110, 97, 45, 98, 111, 116, 46, 116, 111, 112, 47}));
         return replayInfo;
     }
 }
