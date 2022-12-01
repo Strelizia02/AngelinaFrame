e1b34b178db5aaf23f5d01a3ba31d5d3bf9062fd
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-01 15:14:49 +0800

    增强callback方法

--- src/main/java/top/angelinaBot/container/AngelinaListener.java
+++ src/main/java/top/angelinaBot/container/AngelinaListener.java
@@ -41,7 +41,22 @@ public abstract class AngelinaListener {
         AngelinaListener();
     }
 
+    /**
+     * 抽象callback方法，用于对监听消息的判定
+     * 返回值boolean就是是否符合你想要监听的消息
+     */
     public abstract boolean callback(MessageInfo message);
+    
+     /**
+     * callback的增强方法，除callback本身的实现外，引入了对群号的判定
+     */
+    public boolean callbackUp(MessageInfo message) {
+        if (groupId == null) {
+            return callback(message);
+        } else {
+            return groupId.equals(message.getGroupId()) && callback(message);
+        }
+    }
 
     public Long getQq() {
         return qq;
