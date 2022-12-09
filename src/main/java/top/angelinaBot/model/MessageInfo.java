c3ca3f18f1343689620ccecb7005ee704d5eb5e8
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-09 17:07:08 +0800

    补充java doc

--- src/main/java/top/angelinaBot/model/MessageInfo.java
+++ src/main/java/top/angelinaBot/model/MessageInfo.java
@@ -19,37 +19,69 @@ import java.util.List;
  * qq消息格式化Bean
  **/
 public class MessageInfo {
-    //登录qq
+    /**
+     * 登录qq
+    */
     private Long loginQq;
-    //原文字消息
+    /**
+     * 原文字消息
+    */
     private String text;
-    //文字消息的第一节
+    /**
+     * 文字消息的第一节
+    */
     private String keyword;
-    //文字消息的参数
+    /**
+     * 文字消息的参数
+    */
     private List<String> args = new ArrayList<>();
-    //发送人qq
+    /**
+     * 发送人qq
+    */
     private Long qq;
-    //发送人昵称
+    /**
+     * 发送人昵称
+    */
     private String name;
-    //群号
+    /**
+     * 群号
+    */
     private Long groupId;
-    //图片url集合
+    /**
+     * 图片Url合集
+    */
     private List<String> imgUrlList = new ArrayList<>();
-    //图片类型集合
+    /**
+     * 图片类型合集
+    */
     private List<ImageType> imgTypeList = new ArrayList<>();
-    //是否被呼叫
+    /**
+     * 是否被呼叫
+    */
     private Boolean isCallMe = false;
-    //艾特了哪些人
+    /**
+     * 艾特了哪些人
+    */
     private List<Long> atQQList = new ArrayList<>();
-    //发送时间戳
+    /**
+     * 发送时间戳
+    */
     private Integer time;
-    //消息缩略字符串
+    /**
+     * 消息缩略字符串
+    */
     private String eventString;
-    //接收到的事件
+    /**
+     * 接收到的事件
+    */
     private EventEnum event;
-    //是否要发送消息
+    /**
+     * 是否要发送消息
+    */
     private Boolean isReplay = true;
-    //用户权限
+    /**
+     * 用户所拥有的最高权限
+    */
     private PermissionEnum  userAdmin;
 
     public MessageInfo() {
