c6f9ac676659234c30ccd76086aa250f2ebf7773
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-09 15:46:36 +0800

    权限注解

--- src/main/java/top/angelinaBot/annotation/Permission.java
+++ src/main/java/top/angelinaBot/annotation/Permission.java
@@ -1,4 +1,11 @@
 package top.angelinaBot.annotation;
 
+/**
+ * Angelina框架的功能权限注解
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface Permission {
+    PermissionEnum permission() default PermissionEnum.GroupAdministrator;
 }
