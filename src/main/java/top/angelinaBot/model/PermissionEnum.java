6443c5ab13cb406258bd0660a0fe248eed9a07c9
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2022-12-09 15:42:26 +0800

    权限枚举类

---
+++ src/main/java/top/angelinaBot/model/PermissionEnum.java
@@ -0,0 +1,23 @@
+public enum PermissionEnum {
+    Administrator("超级管理员", 3),
+    GroupMaster("群主", 2),
+    GroupAdministrator("群管理", 1),
+    GroupUser("普通成员", 0);
+
+    private String name;
+    private int level;
+
+
+    PermissionEnum(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
