a5b321b58a0ffa4e8a888989f5598c6ad5acec4d
Strelitzia <48582013+Strelizia02@users.noreply.github.com>
2023-02-15 11:48:48 +0800

    小重构ProcessBean，缩小全复杂度

--- src/main/java/top/angelinaBot/bean/AngelinaBeanPostProcessor.java
+++ src/main/java/top/angelinaBot/bean/AngelinaBeanPostProcessor.java
@@ -14,7 +14,6 @@ import top.angelinaBot.model.EventEnum;
 import top.angelinaBot.model.MessageInfo;
 import top.angelinaBot.model.ReplayInfo;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Set;
@@ -27,10 +26,10 @@ import java.util.Set;
 public class AngelinaBeanPostProcessor implements BeanPostProcessor {
 
     /**
-     * 全量扫描所有Bean，查找每一个被@AngelinaGroup注解的方法。
-     * 1.加载所有参数到到KeyWordsMap中
+     * 全量扫描所有Bean，查找每一个被@Angelina注解的方法。
+     * 1.检查方法参数是否符合要求
      * 2.检查keyWords是否有重复
-     * 3.检查方法参数是否符合要求
+     * 3.加载所有参数到到AngelinaContainer中
      */
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
@@ -40,92 +39,104 @@ public class AngelinaBeanPostProcessor implements BeanPostProcessor {
         methods.addAll(MethodUtils.getMethodsListWithAnnotation(clazz, AngelinaGroup.class));
         methods.addAll(MethodUtils.getMethodsListWithAnnotation(clazz, AngelinaFriend.class));
         methods.addAll(MethodUtils.getMethodsListWithAnnotation(clazz, AngelinaEvent.class));
+
         for (Method method: methods) {
             if (method.getReturnType() != ReplayInfo.class) {
                 throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 的返回值类型应为 ReplayGroupInfo.class");
-            } else {
-                // 获取方法参数
-                Class<?>[] parameterTypes = method.getParameterTypes();
-                if (parameterTypes.length != 1) {
-                    throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 的参数个数应为 1 个");
-                } else {
-                    if (parameterTypes[0] != MessageInfo.class) {
-                        throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 参数类型不对，正确参数类型应为 MessageInfo.class");
-                    } else {
-                        for (Annotation annotation : method.getDeclaredAnnotations()) {
-                            //获取方法上的所有修饰注解，循环找到Angelina注解
-                            if (annotation instanceof AngelinaGroup) {
-                                //获取文字关键字
-                                String[] groupKeyWords = ((AngelinaGroup) annotation).keyWords();
-                                //取keyWords中第一个作为唯一方法名标记
-                                String name = groupKeyWords[0];
-                                
-                                for (String keyWord : groupKeyWords) {
-                                    //判断关键字是否重复
-                                    if (AngelinaContainer.groupFuncNameMap.containsKey(keyWord)) {
-                                        Method replaceMethod = AngelinaContainer.groupMap.get(AngelinaContainer.groupFuncNameMap.get(keyWord));
-                                        throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 关键字 \"" + keyWord + "\" 与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() 关键字 \"" + keyWord + "\" 重复");
-                                    } else {
-                                        AngelinaContainer.groupFuncNameMap.put(keyWord, name);
-                                    }
-                                }
-                                //关闭安全检查提升反射速度
-                                method.setAccessible(true);
-                                //确认完全符合要求后，将关键字和方法添加至全局变量keyWordsMap中
-                                AngelinaContainer.groupMap.put(name, method);
+                return null;
+            }
 
-                                String[] dHashList = ((AngelinaGroup) annotation).dHash();
-                                if (AngelinaContainer.dHashMap.size() == 10) {
-                                    log.error("=====dHash 方法超过十个,会一定程度影响性能=====");
-                                }
-                                for (String dHash : dHashList) {
-                                    //判断DHash是否重复
-                                    if (!"".equals(dHash) && AngelinaContainer.dHashMap.containsKey(dHash)) {
-                                        Method replaceMethod = AngelinaContainer.dHashMap.get(dHash);
-                                        throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() DHash与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() DHash重复");
-                                    } else {
-                                        //关闭安全检查提升反射速度
-                                        method.setAccessible(true);
-                                        //确认完全符合要求后，将关键字和方法添加至全局变量dHashMap中
-                                        AngelinaContainer.dHashMap.put(dHash, method);
-                                    }
-                                }
-                            }
-                            
-                            if (annotation instanceof AngelinaFriend) {
-                                String[] friendKeyWords = ((AngelinaFriend) annotation).keyWords();
-                                for (String keyWord : friendKeyWords) {
-                                    //判断关键字是否重复
-                                    if (AngelinaContainer.friendMap.containsKey(keyWord)) {
-                                        Method replaceMethod = AngelinaContainer.friendMap.get(keyWord);
-                                        throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 关键字 \"" + keyWord + "\" 与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() 关键字 \"" + keyWord + "\" 重复");
-                                    } else {
-                                        //关闭安全检查提升反射速度
-                                        method.setAccessible(true);
-                                        //确认完全符合要求后，将关键字和方法添加至全局变量friendMap中
-                                        AngelinaContainer.friendMap.put(keyWord, method);
-                                    }
-                                }
-                            }
+            // 获取方法参数
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            if (parameterTypes.length != 1) {
+                throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 的参数个数应为 1 个");
+                return null;
+            }
 
-                            if (annotation instanceof AngelinaEvent) {
-                                EventEnum eventEnum = ((AngelinaEvent) annotation).event();
-                                //判断关键字是否重复
-                                if (AngelinaContainer.eventMap.containsKey(eventEnum)) {
-                                    Method replaceMethod = AngelinaContainer.eventMap.get(eventEnum);
-                                    throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 事件 \"" + eventEnum + "\" 与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() 事件 \"" + eventEnum + "\" 重复");
-                                } else {
-                                    //关闭安全检查提升反射速度
-                                    method.setAccessible(true);
-                                    //确认完全符合要求后，将关键字和方法添加至全局变量eventMap中
-                                    AngelinaContainer.eventMap.put(eventEnum, method);
-                                }
-                            }
-                        }
-                    }
-                }
+            if (parameterTypes[0] != MessageInfo.class) {
+                throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 参数类型不对，正确参数类型应为 MessageInfo.class");
+                return null;
+            }
+
+            //关闭安全检查提升反射速度
+            method.setAccessible(true);
+
+            AngelinaGroup angelinaGroup = method.getAnnotation(AngelinaGroup.class);
+            if (angelinaGroup != null) {
+                checkOutAngelinaGroup(angelinaGroup, method, clazz);
+            }
+
+            AngelinaFriend angelinaFriend = method.getAnnotation(AngelinaFriend.class);
+            if (angelinaFriend != null) {
+                checkOutAngelinaFriend(angelinaFriend, method, clazz);
+            }
+
+            AngelinaEvent angelinaEvent = method.getAnnotation(AngelinaEvent.class);
+            if (angelinaEvent != null) {
+                checkOutAngelinaEvent(angelinaEvent, method, clazz);
             }
         }
         return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+
+    private void checkOutAngelinaGroup(AngelinaGroup annotation, Method method, Class<?> clazz) {
+        //获取文字关键字
+        String[] groupKeyWords = annotation.keyWords();
+        //取keyWords中第一个作为唯一方法名标记
+        String name = groupKeyWords[0];
+
+        for (String keyWord : groupKeyWords) {
+            //判断关键字是否重复
+            if (AngelinaContainer.groupFuncNameMap.containsKey(keyWord)) {
+                Method replaceMethod = AngelinaContainer.groupMap.get(AngelinaContainer.groupFuncNameMap.get(keyWord));
+                throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 关键字 \"" + keyWord + "\" 与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() 关键字 \"" + keyWord + "\" 重复");
+                return;
+            }
+            AngelinaContainer.groupFuncNameMap.put(keyWord, name);
+        }
+        //确认完全符合要求后，将关键字和方法添加至全局变量groupMap中
+        AngelinaContainer.groupMap.put(name, method);
+
+        String[] dHashList = annotation.dHash();
+        if (AngelinaContainer.dHashMap.size() == 10) {
+            log.error("=====dHash 方法超过十个,会一定程度影响性能=====");
+        }
+
+        for (String dHash : dHashList) {
+            //判断DHash是否重复
+            if (!"".equals(dHash) && AngelinaContainer.dHashMap.containsKey(dHash)) {
+                Method replaceMethod = AngelinaContainer.dHashMap.get(dHash);
+                throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() DHash与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() DHash重复");
+                return;
+            }
+            //确认完全符合要求后，将关键字和方法添加至全局变量dHashMap中
+            AngelinaContainer.dHashMap.put(dHash, method);
+        }
+    }
+
+    private void checkOutAngelinaFriend(AngelinaFriend annotation, Method method, Class<?> clazz) {
+        String[] friendKeyWords = annotation.keyWords();
+        for (String keyWord : friendKeyWords) {
+            //判断关键字是否重复
+            if (AngelinaContainer.friendMap.containsKey(keyWord)) {
+                Method replaceMethod = AngelinaContainer.friendMap.get(keyWord);
+                throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 关键字 \"" + keyWord + "\" 与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() 关键字 \"" + keyWord + "\" 重复");
+                return;
+            }
+            //确认完全符合要求后，将关键字和方法添加至全局变量friendMap中
+            AngelinaContainer.friendMap.put(keyWord, method);
+        }
+    }
+
+    private void checkOutAngelinaEvent(AngelinaEvent annotation, Method method, Class<?> clazz) {
+        EventEnum eventEnum = annotation.event();
+        //判断关键字是否重复
+        if (AngelinaContainer.eventMap.containsKey(eventEnum)) {
+            Method replaceMethod = AngelinaContainer.eventMap.get(eventEnum);
+            throw new AngelinaException(clazz + " 的方法 " + method.getName() + "() 事件 \"" + eventEnum + "\" 与 " + replaceMethod.getDeclaringClass().getName() + " 的方法 " + replaceMethod.getName() + "() 事件 \"" + eventEnum + "\" 重复");
+            return;
+        }
+        //确认完全符合要求后，将关键字和方法添加至全局变量eventMap中
+        AngelinaContainer.eventMap.put(eventEnum, method);
     }
 }
